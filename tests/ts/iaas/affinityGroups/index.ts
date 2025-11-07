5f0ab0866e05b6282726fc208028f36754127377
Alexander Dahmen <alexander.dahmen@inovex.de>
2025-11-07 11:12:37 +0100

    Add affinityGroup TypeScript unit tests
    Signed-off-by: Alexander Dahmen <alexander.dahmen@inovex.de>

--- tests/ts/iaas/affinityGroups/index.ts
+++ tests/ts/iaas/affinityGroups/index.ts
@@ -5,9 +5,17 @@ export const affinityGroupProjectId = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx";
 export const affinityGroupName = "example-affinity-group-name";
 export const affinityGroupPolicy = "hard-anti-affinity";
 
-// Add input here which should be tested
+export const affinityGroupId = "affinity-group-id-to-read";
+
+// only required fields were set
 export const exampleAffinityGroup = new stackit.AffinityGroup("example", {
     projectId: affinityGroupProjectId,
     name: affinityGroupName,
     policy: affinityGroupPolicy,
+});
+
+// datasource
+export const affinityGroupDatasource = stackit.getAffinityGroupOutput({
+    projectId: affinityGroupProjectId,
+    affinityGroupId: affinityGroupId,
 });
--- tests/ts/iaas/affinityGroups/test.ts
+++ tests/ts/iaas/affinityGroups/test.ts
@@ -1,8 +1,7 @@
-// affinitygroup.test.ts
 import * as assert from "assert";
 import * as pulumi from "@pulumi/pulumi";
 import "mocha";
-import { affinityGroupProjectId, affinityGroupName, affinityGroupPolicy } from "./index";
+import { affinityGroupProjectId, affinityGroupName, affinityGroupPolicy, affinityGroupId, exampleAffinityGroup } from "./index";
 
 pulumi.runtime.setMocks({
     newResource: function(args: pulumi.runtime.MockResourceArgs): {id: string, state: any} {
@@ -12,6 +11,15 @@ pulumi.runtime.setMocks({
         };
     },
     call: function(args: pulumi.runtime.MockCallArgs) {
+        // We check the token to identify which data source is being called.
+        if (args.token === "stackit:index/getAffinityGroup:getAffinityGroup") {
+            // Check if the input parameters were passed correctly
+            if (args.inputs.projectId !== affinityGroupProjectId || args.inputs.affinityGroupId !== affinityGroupId) {
+                throw new Error("getAffinityGroup call received incorrect input parameters.");
+            }
+            // Return the complete object
+            return { ...exampleAffinityGroup, ...args.inputs };
+        }
         return args.inputs;
     },
 },
@@ -31,7 +39,7 @@ describe("exampleAffinityGroup", () => {
 
     it("affinityGroup must have a project id", function(done) {
         pulumi.all([infra.exampleAffinityGroup.urn, infra.exampleAffinityGroup]).apply(([urn, affinityGroup]) => {
-            if (!affinityGroup || !affinityGroup["projectId"]) {
+            if (!affinityGroup?.projectId) {
                 done(new Error(`Missing projectId tag on affinityGroup ${urn}`));
             } else {
                 done();
@@ -41,7 +49,7 @@ describe("exampleAffinityGroup", () => {
 
     it("affinityGroup must have a name", function(done) {
         pulumi.all([infra.exampleAffinityGroup.urn, infra.exampleAffinityGroup]).apply(([urn, affinityGroup]) => {
-            if (!affinityGroup || !affinityGroup["name"]) {
+            if (!affinityGroup?.name) {
                 done(new Error(`Missing a name tag on affinityGroup ${urn}`));
             } else {
                 done();
@@ -51,7 +59,7 @@ describe("exampleAffinityGroup", () => {
 
      it("affinityGroup must have a policy", function(done) {
         pulumi.all([infra.exampleAffinityGroup.urn, infra.exampleAffinityGroup]).apply(([urn, affinityGroup]) => {
-            if (!affinityGroup || !affinityGroup["policy"]) {
+            if (!affinityGroup?.policy) {
                 done(new Error(`Missing a policy tag on affinityGroup ${urn}`));
             } else {
                 done();
@@ -85,6 +93,55 @@ describe("exampleAffinityGroup", () => {
                 done();
             } else {
                 done(new Error(`Provided policy ${policy} was not set correctly on affinityGroup ${urn}`));
+            }
+        });
+    });
+});
+
+describe("affinityGroup datasource test", () => {
+    let infra: typeof import("./index");
+
+    before(async function() {
+        // It's important to import the program _after_ the mocks are defined.
+        infra = await import("./index");
+    })
+
+    it("check if projectId was correctly set", function(done) {
+        pulumi.all([infra.affinityGroupDatasource, infra.affinityGroupDatasource.projectId]).apply(([urn, projectId]) => {
+            if (projectId === affinityGroupProjectId) {
+                done();
+            } else {
+                done(new Error(`Provided policy ${projectId} was not set correctly on affinityGroup ${urn.name}`));
+            }
+        });
+    });
+
+    it("check if affinityGroupId was correctly set", function(done) {
+        pulumi.all([infra.affinityGroupDatasource, infra.affinityGroupDatasource.affinityGroupId]).apply(([urn, affinityGroupId]) => {
+            if (affinityGroupId === infra.affinityGroupId) {
+                done();
+            } else {
+                done(new Error(`Provided affinityGroupId ${affinityGroupId} was not set correctly on affinityGroup ${urn.name}`));
+            }
+        });
+    });
+
+    it("check if name was correctly set", function(done) {
+        pulumi.all([infra.affinityGroupDatasource, infra.affinityGroupDatasource.name]).apply(([urn, name]) => {
+            if (name === affinityGroupName) {
+                done();
+            } else {
+                done(new Error(`Provided name ${name} was not set correctly on affinityGroup ${urn.name}`));
+            }
+        });
+    });
+
+    it("check if policy was correctly set", function(done) {
+        pulumi.all([infra.affinityGroupDatasource, infra.affinityGroupDatasource.policy]).apply(([urn, policy]) => {
+            if (policy === affinityGroupPolicy) {
+                done();
+            } else {
+                done(new Error(`Provided policy ${policy} was not set correctly on affinityGroup ${urn.name}`));
             }
         });
     });
